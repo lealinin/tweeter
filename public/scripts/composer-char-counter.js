0631dbcbf15460ec9f6b4e6784434787787f0da0
Lea Linin <lea.linin@gmail.com>
2019-12-18 00:29:28 +0000

    Added character counting function with jQuery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log('here');
+  $('textarea').keyup(function() {
+    console.log('test');
+    let length = $(this).val().length;
+    let counter = 140;
+    if (counter - length < 0) {
+      $('.counter').css("color", "#DC143C");
+    } else {
+      $('.counter').css("color", "#545149");
+    }
+    $('.counter').text(counter - length);
+  })
+});
